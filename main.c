ae7c96f4f8ceb0a2dba07e0f8fc314aea43ff751
sugunjenk <102678449+sugunjenk@users.noreply.github.com>
2022-04-05 23:31:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 #include "lib/sample_header.h"
 
 int main(){
-    int a = 1, b = 2;
-    printf("%d", sum(a,b));
+   int pilihan;
+    do{
+        printf("Main Menu\n");
+        printf("1. Tambah Data Buku\n");
+        printf("2. Edit Data Buku\n");
+        printf("3. Hapus Data\n");
+        printf("4. Lihat Data\n");
+        printf("Keluar Program\n");
+    }
+    while (0);
     return 0;
 }

cdbe9dcd473ec1bbe53b7010c2cb30b88e49bed7
Putu Widyantara <putuwaw973@gmail.com>
2022-04-10 11:32:05 +0800

    Create CRUD header

---
+++ lib/crud.h
@@ -0,0 +1,39 @@
+#ifndef CRUD_H_INCLUDED
+#define CRUD_H_INCLUDED
+
+// C STANDARD LIBRARY
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+
+// GLOBAL VARIABLE
+typedef struct book{
+    char name[100];
+    char author[100];
+    int year;
+    char category[100];
+    char created[100];
+    char modified[100];
+    struct book *next;
+} book;
+
+book *b, *head;
+
+time_t rawtime;
+struct tm *timeInfo;
+
+// SFML
+#include "SFML/Graphics.h"
+#include "SFML/Window.h"
+
+// LOCAL HEADER
+#include "create.h"
+
+// FUNCTIONS
+void clearScreen();
+void pauseScreen();
+char *timeToStr(struct tm *timeInfo);
+
+#endif // CRUD_H_INCLUDED

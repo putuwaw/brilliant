a80ab9c3c094cad719a8d75a60e142274f603af9
sugunjenk <102678449+sugunjenk@users.noreply.github.com>
2022-04-13 06:32:56 +0700

    Update main.c
    I have edited the main menu view

--- main.c
+++ main.c
@@ -7,14 +7,18 @@ int main(){
     do{
         repeat = true;
         system("cls");
-        printf("Main Menu\n");
-        printf("0. About\n");
-        printf("1. Tambah Data Buku\n");
-        printf("2. Edit Data Buku\n");
-        printf("3. Hapus Data\n");
-        printf("4. Lihat Data\n");
-        printf("5. Keluar Program\n");
-        printf("Pilihan Anda: ");
+    printf("||====================================================================||\n");
+    printf("||============================ Main Menu =============================||\n");
+    printf("||====================================================================||\n");
+    printf("|| Pilihan Menu :                                                     ||\n");
+    printf("||====================================================================||\n");
+    printf("|| 0. About                                                           ||\n");
+    printf("|| 1. Tambah Data Buku                                                ||\n");
+    printf("|| 2. Edit Data Buku                                                  ||\n");
+    printf("|| 3. Hapus Data                                                      ||\n");
+    printf("|| 4. Lihat Data                                                      ||\n");
+    printf("|| 5. Keluar Progam                                                   ||\n");
+    printf("||====================================================================||\n");
         scanf("%d", &pilihan);
         switch (pilihan){
             case 0:
